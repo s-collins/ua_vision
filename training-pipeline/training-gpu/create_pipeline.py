03949919ca42d58f5bd55f3de9364340be0e0093
Sean Collins <sgc29@zips.uakron.edu>
2019-02-27 03:31:56 -0500

    Finally succeed with merge function

--- training-pipeline/training-gpu/create_pipeline.py
+++ training-pipeline/training-gpu/create_pipeline.py
@@ -32,14 +32,17 @@ def populate_config(settings):
     print 'SUCCESS'
 
     print '...Updating config settings',
-
-    # TODO: update the config settings
-    #configs['model'].ssd.num_classes = settings['config']['model']['num_classes']
     hparams = tf.contrib.training.HParams(
         **{
-            "model.ssd.num_classes": 1
+            "model.ssd.num_classes": 1,
+            "train_config.fine_tune_checkpoint": settings['config']['train_config']['fine_tune_checkpoint'],
+            "train_config.num_steps": settings['config']['train_config']['num_steps'],
+            "eval_config.num_examples": settings['config']['eval_config']['num_examples'],
+            "label_map_path": settings['config']['label_map_path']
         })
     configs = config_util.merge_external_params_with_configs(configs, hparams)
+    configs['train_input_config'].tf_record_input_reader.input_path[0] = settings['config']['train_input_reader']['tf_record_input_reader']['input_path']
+    configs['eval_input_config'].tf_record_input_reader.input_path[0] = settings['config']['eval_input_reader']['tf_record_input_reader']['input_path']
     print 'SUCCESS'
 
     print '...Writing new config file',
@@ -50,20 +53,20 @@ def populate_config(settings):
 
 if __name__ == '__main__':
     # Read settings file
-    print '[[ Loading settings ]]'
+    print 'Loading settings'
     settings = settings.load('settings.yaml')
 
     # Create directories
-    print '[[ Creating directories ]]'
+    print 'Creating directories'
     for key, directory in settings['dirs'].iteritems():
         if not os.path.exists(directory):
             os.makedirs(directory)
 
     # Download the base model
-    print '[[ Downloading the base model ]]'
+    print 'Downloading the base model'
     download_base_model(settings)
 
     # Populate the pipeline.config file
-    print '[[ Generating "pipeline.config" ]]'
+    print 'Generating "pipeline.config"'
     populate_config(settings)
 
