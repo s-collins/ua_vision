c2e56815964339c4ef2b1a1cb03ff22662dae290
Sean Collins <sgc29@zips.uakron.edu>
2019-03-06 13:17:05 -0500

    Add directory structure for prototype

--- collect/collection/training_example.py
+++
@@ -1,8 +0,0 @@
-class TrainingExample:
-
-    def __init__(self):
-        self.camera_angle = None
-        self.camera_height = None
-        self.light_angle = None
-        self.image = None
-        self.labels = None
